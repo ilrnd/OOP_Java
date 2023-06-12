bfbaab0109e69e90e99b58398a431827c5598ea0
Ilya Skripnikov <scripnikovs@list.ru>
2023-06-13 00:33:47 +0300

    Added HT after Lesson5

---
+++ lesson5/src/main/java/ru/geekbrains/lesson5/Program.java
@@ -0,0 +1,38 @@
+package ru.geekbrains.lesson5;
+
+import ru.geekbrains.lesson5.models.TableModel;
+import ru.geekbrains.lesson5.presenters.BookingPresenter;
+import ru.geekbrains.lesson5.presenters.Model;
+import ru.geekbrains.lesson5.presenters.View;
+import ru.geekbrains.lesson5.views.BookingView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+
+public class Program {
+
+    /**
+     * TODO: Домашняя работа: Метод changeReservationTable ДОЛЖЕН ЗАРАБОТАТЬ!
+     * @param args
+     */
+    public static void main(String[] args) {
+        Model tableModel = new TableModel();
+        BookingView bookingView = new BookingView();
+        BookingPresenter bookingPresenter = new BookingPresenter(tableModel, bookingView);
+        bookingPresenter.showTables();
+
+        // Клиент нажимает на кнопку "Зарезервировать", возбуждается событие,
+        // вызывается метод reservationTable()
+
+        bookingView.reservationTable(new Date(), 102, "Станислав");
+        bookingView.reservationTable(new Date(), 105, "Петр");
+
+
+        bookingView.changeReservationTable(9001, new Date(), 103, "Илья");
+        bookingView.changeReservationTable(9006, new Date(), 103, "Илья");
+
+
+    }
+
+}
---
+++ lesson5/src/main/java/ru/geekbrains/lesson5/presenters/Model.java
@@ -0,0 +1,19 @@
+package ru.geekbrains.lesson5.presenters;
+
+import ru.geekbrains.lesson5.models.Table;
+
+import java.util.Collection;
+import java.util.Date;
+
+public interface Model {
+    Collection<Table> loadTables();
+
+    /**
+     * Бронирование столика
+     * @param reservationDate Дата бронирования
+     * @param tableNo Номер столика
+     * @param name Имя клиента
+     */
+    int reservationTable(Date reservationDate, int tableNo, String name);
+    int changeReservationTable(int oldReservationId, Date reservationDate, int tableNo, String name);
+}
